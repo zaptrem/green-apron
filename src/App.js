8182ec7edc1d32fb25d794f8d779194f8f2c4b52
armita-jamshidi <aaj34@cornell.edu>
2023-03-19 13:12:00 -0400

    Work-inprogress: adding navigation bar

--- src/App.js
+++ src/App.js
@@ -13,6 +13,27 @@ function App() {
   const [foodList, setFoodList] = useState(false);
   return (
     <div className="app">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Green Apr</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="home" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="login" href="#">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+      <button class="button"> Help mitigate food waste </button>
       <header className="App-header">
         <div className="logo">
           <img src={logo} className="App-logo" alt="logo" />
